3a6444559ed05db761b812c298e26de53ef45267
Adrien Pauchet <grandchefdotkong@gmail.com>
2023-03-02 15:09:28 +0900

    login added

--- src/controllers/authentification.ts
+++ src/controllers/authentification.ts
@@ -36,3 +36,45 @@ export const register = async (req: express.Request, res: express.Response) => {
 
   return res.sendStatus(500);
 }
+
+
+export const login = async (req: express.Request, res: express.Response) => {
+
+  try {
+
+    const { email, password } = req.body;
+    if(!email || !password) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserByEmail(email)
+      .select('+authentification.salt +authentification.password');
+
+    if(!user || user?.authentification?.salt === undefined) {
+      return res.sendStatus(400);
+    }
+
+    const expectedHash = authentification(user.authentification.salt, password);
+    if(user.authentification.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentification.sessionToken = authentification(salt, user._id.toString());
+
+    await user.save();
+    res.cookie('ADORI-AUTH', user.authentification.sessionToken, 
+      { domain: 'localhost', path: '/' });
+
+    return res.status(200).json(user).end();
+
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+
+  return res.sendStatus(400);
+}
+
+
+
--- src/router/authentification.ts
+++ src/router/authentification.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 
-import { register } from '../controllers/authentification';
+import { register, login } from '../controllers/authentification';
 
 export default(router: express.Router) => {
   router.post('/auth/register', register);
+  router.post('/auth/login', login);
 }
